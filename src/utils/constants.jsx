f92e7b5212319aa90141c7cbb10480377d7d872f
Mahendra2611 <stpmpv30@gmail.com>
2025-02-16 19:34:21 +0530

    backend link

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,4 @@
-//export const USER_END_POINT ="http://localhost:3000/user"
+export const USER_END_POINT ="https://vercel.com/stpmpv30-gmailcoms-projects/rojgaar-backend/EKajgGWL8HRbGQSK82ZBCD8pMv1D"
  //export const END_POINT = "https://rojgaar-1.onrender.com"
-export const END_POINT = "http://localhost:3000"
+ 
+//export const END_POINT = "http://localhost:3000"
