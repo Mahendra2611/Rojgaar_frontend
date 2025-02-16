49ea3a13fc12835d6a3d2dabf03a09f94dbc5ccf
Mahendra2611 <stpmpv30@gmail.com>
2025-02-16 19:46:42 +0530

    .

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const END_POINT ="https://vercel.com/stpmpv30-gmailcoms-projects/rojgaar-backend/EKajgGWL8HRbGQSK82ZBCD8pMv1D"
- //export const END_POINT = "https://rojgaar-1.onrender.com"
+//export const END_POINT ="https://vercel.com/stpmpv30-gmailcoms-projects/rojgaar-backend/EKajgGWL8HRbGQSK82ZBCD8pMv1D"
+ export const END_POINT = "https://rojgaar-backend.vercel.app"
  
 //export const END_POINT = "http://localhost:3000"
