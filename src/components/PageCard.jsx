cc47b1508c737a08c783600cdbc62a60467d62c8
Mahendra2611 <stpmpv30@gmail.com>
2025-02-16 23:14:33 +0530

    Page Card

--- src/components/PageCard.jsx
+++ src/components/PageCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { PrimaryButton } from "./PrimaryButton";
+import { Link } from "react-router-dom";
 const PageCard = ({ title, path, bgColor }) => {
   const navigate = useNavigate();
 
@@ -9,9 +10,12 @@ const PageCard = ({ title, path, bgColor }) => {
       className={`p-6 rounded-2xl shadow-lg text-white bg-teal-600  hover:scale-105 transition-transform duration-300 ease-in-out w-full max-w-[300px] min-h-[150px] flex flex-col justify-between`}
     >
       <h2 className="text-2xl font-blackOps  text-center mb-4">{title}</h2>
-      <PrimaryButton onClick={() => navigate(path)}>
+      <Link to={path}>
+      <PrimaryButton>
         Visit {title}
       </PrimaryButton>
+      </Link>
+     
     </div>
   );
 };
